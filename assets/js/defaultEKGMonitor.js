540132e87c4d1edf294d9b59c466d3e102955080
Erkin Otles <eotles@gmail.com>
2023-12-09 16:07:08 -0500

    Update defaultEKGMonitor.js

--- assets/js/defaultEKGMonitor.js
+++ assets/js/defaultEKGMonitor.js
@@ -16,15 +16,19 @@ import { EKGWaveform } from './EKGWaveform.js';
 import { WaveformPlotter } from './waveformPlotter.js';
 
 export function DefaultEKGMonitor(EKGParameters, canvasId = 'EKGMonitor') {
-    // Create the canvas element
-    const canvas = document.createElement('canvas');
-    canvas.id = canvasId;
-    canvas.width = 500;
-    canvas.height = 200;
+    let canvas = document.getElementById(canvasId);
 
-    // Append the canvas to the body or a specific container
-    document.body.appendChild(canvas);
+    // If the canvas doesn't exist, create it
+    if (!canvas) {
+        canvas = document.createElement('canvas');
+        canvas.id = canvasId;
+        canvas.width = 500;
+        canvas.height = 200;
 
+        // Append the canvas to the body or a specific container
+        document.body.appendChild(canvas);
+    }
+    
     const seconds = 5;
     const yMin = 149;
     const yMax = 152;
